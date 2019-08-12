795f5ea520541685c1ad6227da3fbce432fd99a1
hvleifsson <46728121+hvleifsson@users.noreply.github.com>
2019-08-12 09:44:18 +0200

    Create Clear_local.bat

---
+++ Clear_local.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+:start
+SET choice=
+SET /p choice=Delete files in FOLDER PATH ? [Y/N]: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='Y' GOTO yes
+IF '%choice%'=='y' GOTO yes
+IF '%choice%'=='N' GOTO no
+IF '%choice%'=='n' GOTO no
+IF '%choice%'=='' GOTO no
+ECHO "%choice%" is not valid
+ECHO.
+GOTO start
+
+:no
+echo Nothing has been deleted. 
+PAUSE
+EXIT
+
+:yes
+echo Deleting files in C:\Vault Purac\Workspace
+set folder="C:\Vault Purac\Workspace"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+echo Done!
+PAUSE
+EXIT
+
+PAUSE
